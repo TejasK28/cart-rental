4bae2e393ebaac8912563538c25149461dff0da0
Tejas <tejaskandri28@gmail.com>
2024-10-31 21:16:09 -0400

    vercel

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 //app.use(cors());
 app.use(express.json());
-app.use(cors({ origin: '*' }));
+app.use(cors({ origin: ['https://cart-rental-gqqj.vercel.app/', 'http://localhost:5001'] }));
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
